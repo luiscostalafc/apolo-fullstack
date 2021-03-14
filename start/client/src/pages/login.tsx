7d489110cbdf6302a7c0fa52c180f25235d30316
luiscostalafc <luiscostalafc@gmail.com>
2021-03-14 18:14:42 -0300

    feat: update cache

--- start/client/src/pages/login.tsx
+++ start/client/src/pages/login.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { gql, useMutation } from '@apollo/client';
 
+import { isLoggedInVar } from '../cache'
+
 import { LoginForm, Loading } from '../components';
 import * as LoginTypes from './__generated__/login';
 
@@ -22,6 +24,7 @@ export default function Login() {
       if (login) {
         localStorage.setItem('token', login.token as string);
         localStorage.setItem('userId', login.id as string);
+        isLoggedInVar(true);
       }
     }
   });
