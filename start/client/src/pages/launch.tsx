71227087820b21f20e6d4b956710fa9f8c99c3f3
luiscostalafc <luiscostalafc@gmail.com>
2021-03-14 19:07:15 -0300

    feat: create logout-button

--- start/client/src/pages/launch.tsx
+++ start/client/src/pages/launch.tsx
@@ -25,32 +25,30 @@ interface LaunchProps extends RouteComponentProps {
 }
 
 const Launch: React.FC<LaunchProps> = ({ launchId }) => {
-   const {
-     data,
-     loading,
-     error,
-   } = useQuery<LaunchDetailsTypes.LaunchDetails, LaunchDetailsTypes.LaunchDetailsVariables
-   >(GET_LAUNCH_DETAILS,
-    { variables: { launchId }}
-    );
+  const {
+    data,
+    loading,
+    error,
+  } = useQuery<
+    LaunchDetailsTypes.LaunchDetails,
+    LaunchDetailsTypes.LaunchDetailsVariables
+  >(GET_LAUNCH_DETAILS,
+    { variables: { launchId } }
+  );
 
-    if (loading) return <Loading />
-    if (error) return <p>ERROR: {error.message}</p>;
-    if (!data) return <p>Not found</p>;
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR: {error.message}</p>;
+  if (!data) return <p>Not found</p>;
 
-    return (
-      <Fragment>
-        <Header
-        image={
-          data.launch && 
-          data.launch.mission && 
-          data.launch.mission.missionPatch}
-        >
-        </Header>
-        <LaunchDetail {...data.launch} />
-        <ActionButton {...data.launch} />
-      </Fragment>
-    );
+  return (
+    <Fragment>
+      <Header image={data.launch && data.launch.mission && data.launch.mission.missionPatch}>
+        {data && data.launch && data.launch.mission && data.launch.mission.name}
+      </Header>
+      <LaunchDetail {...data.launch} />
+      <ActionButton {...data.launch} />
+    </Fragment>
+  );
 }
 
 export default Launch;
